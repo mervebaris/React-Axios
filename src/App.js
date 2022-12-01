78b41dd75acae0d32fdf9761871cbe05931a9ea8
Merve Barış <99658291+mervebaris@users.noreply.github.com>
2022-12-01 16:24:42 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [veri, setVeri] = useState ("");
+  const [id, setId] = useState(0);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => setVeri(res.data[id].title))
+      .catch(err => console.log(err))
+  },[veri,id])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>{ veri }</h1> 
+    <p>
+      <input type="text" value={id} onChange={e => setId(e.target.value)} />
+    </p>
     </div>
   );
 }
